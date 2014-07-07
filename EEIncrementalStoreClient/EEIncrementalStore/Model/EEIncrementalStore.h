e462fe63bb33be3f17a284d373aa6ed8c818bb56
roberto avanzi <avanzir@robertosmacbook.mil.esselunga.net>
2014-07-07 16:30:54 +0200

    commit

--- EEIncrementalStoreClient/EEIncrementalStore/Model/EEIncrementalStore.h
+++ EEIncrementalStoreClient/EEIncrementalStore/Model/EEIncrementalStore.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <AFIncrementalStore/AFIncrementalStore.h>
+#import "AFIncrementalStore.h"
 
 @interface EEIncrementalStore : AFIncrementalStore
 
